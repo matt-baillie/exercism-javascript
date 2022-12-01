7c8f0f6062a95c30898c04382521ad9249b2f807
Matthew Baillie <mbaillie93@gmail.com>
2022-11-30 23:15:32 -0800

    resistor-color exercise

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,36 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (param1) => {
+  console.log(param1);
+
+  return COLORS.indexOf(param1);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+// {
+//   black: 0,
+//   brown: 1,
+//   red: 2,
+//   orange: 3,
+//   yellow: 4,
+//   green: 5,
+//   blue: 6,
+//   violet: 7,
+//   grey: 8,
+//   white: 9,
+// };
