1aa3993f989aee6fbe5de472c5c0611952528b1c
Matthew Baillie <mbaillie93@gmail.com>
2022-12-05 22:47:57 -0800

    Pangram exercise complete

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -3,6 +3,34 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isPangram = (input) => {
+  input = input.toLowerCase();
+  const pangram =
+    input.includes("a") &&
+    input.includes("b") &&
+    input.includes("c") &&
+    input.includes("d") &&
+    input.includes("e") &&
+    input.includes("f") &&
+    input.includes("g") &&
+    input.includes("h") &&
+    input.includes("i") &&
+    input.includes("j") &&
+    input.includes("k") &&
+    input.includes("l") &&
+    input.includes("m") &&
+    input.includes("n") &&
+    input.includes("o") &&
+    input.includes("p") &&
+    input.includes("q") &&
+    input.includes("r") &&
+    input.includes("s") &&
+    input.includes("t") &&
+    input.includes("u") &&
+    input.includes("v") &&
+    input.includes("w") &&
+    input.includes("x") &&
+    input.includes("y") &&
+    input.includes("z");
+  return pangram;
 };
