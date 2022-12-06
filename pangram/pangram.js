e6007086c2141f5dbf0ae1806252f5b4f1969292
Matthew Baillie <mbaillie93@gmail.com>
2022-12-05 22:53:05 -0800

    Pangram exercise refactored

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -5,32 +5,9 @@
 
 export const isPangram = (input) => {
   input = input.toLowerCase();
-  const pangram =
-    input.includes("a") &&
-    input.includes("b") &&
-    input.includes("c") &&
-    input.includes("d") &&
-    input.includes("e") &&
-    input.includes("f") &&
-    input.includes("g") &&
-    input.includes("h") &&
-    input.includes("i") &&
-    input.includes("j") &&
-    input.includes("k") &&
-    input.includes("l") &&
-    input.includes("m") &&
-    input.includes("n") &&
-    input.includes("o") &&
-    input.includes("p") &&
-    input.includes("q") &&
-    input.includes("r") &&
-    input.includes("s") &&
-    input.includes("t") &&
-    input.includes("u") &&
-    input.includes("v") &&
-    input.includes("w") &&
-    input.includes("x") &&
-    input.includes("y") &&
-    input.includes("z");
+  const pangram = [..."abcdefghijklmnopqrstuvwxyz"].every((c) =>
+    input.includes(c)
+  );
+
   return pangram;
 };
