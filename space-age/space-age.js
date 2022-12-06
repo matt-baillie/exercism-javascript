995a30baa857ae70b2e01109654d0bbdf1e761aa
Matthew Baillie <mbaillie93@gmail.com>
2022-12-05 22:32:36 -0800

    Space-age exercise complete

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -3,6 +3,38 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
+export const age = (planet, date) => {
+  let earthAge = date / 31557600;
+  let age;
+  switch (planet) {
+    case "earth":
+      console.log(planet, date);
+      age = earthAge;
+      break;
+    case "mercury":
+      age = earthAge / 0.2408467;
+      break;
+    case "venus":
+      age = earthAge / 0.61519726;
+      break;
+    case "mars":
+      age = earthAge / 1.8808158;
+      break;
+    case "jupiter":
+      age = earthAge / 11.862615;
+      break;
+    case "saturn":
+      age = earthAge / 29.447498;
+      break;
+    case "uranus":
+      age = earthAge / 84.016846;
+      break;
+    case "neptune":
+      age = earthAge / 164.79132;
+      break;
+    default:
+      break;
+  }
+  console.log(age.toFixed(2));
+  return parseFloat(age.toFixed(2));
 };
